5549b26c2284a21698f5b3b63370ab54f9644cd0
briannapinson <blph3y@umsystem.edu>
2026-03-06 11:32:46 -0600

    added images to a few cards

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
 const [currentIndex, setCurrentIndex] = useState(0);
 
 const cards = [
-  { question: "Telnet", answer: "Allows a user on a remote client machine, called the Telnet client, to access the resources of another machine, the Telnet server.", layer: "Application" },
+  { question: "Telnet", answer: "Allows a user on a remote client machine, called the Telnet client, to access the resources of another machine, the Telnet server.", layer: "Application", image: "src/images/TelNet.png"},
   { question: "File Transfer Protocol (FTP)", answer: "Allows you to transfer files across an IP network, and it can accomplish this between any two machines that are using it.", layer: "Application" },
   { question: "Secure File Transfer Protocol (SFTP)", answer: "Used when you need to transfer files over an encrypted connection. It uses SSH which encrypts the connection. Secure FTP.", layer: "Application" },
   { question: "Simple Mail Transfer Protocol (SMTP)", answer: "Used to send mail. Answering our ubiquitous call to email, uses spooled or queued, method of mail delivery.", layer: "Application" },
@@ -19,7 +19,7 @@ const cards = [
   { question: "Simple Network Management Protocol (SNMP)", answer: "Collects and manipulates valuable network information. Used to help monitor networks and inform administrators when there is a problem.", layer: "Application" },
   { question: "Secure Shell (SSH)", answer: "Sets up a Telnet session over a standard TCP/IP connection. Is employed for doing links like logging into other systems, running programs on remote systems, or moving files from one system to another.", layer: "Application" },
   { question: "Hypertext Transfer Protocol (HTTP)", answer: "Manages communication between web browsers and web servers, and opens the right resources when you click on a link.", layer: "Application" },
-  { question: "Domain Name Service (DNS)", answer: "Resolves hostnames, specifically Internet names. Converts domain names into IP addresses.", layer: "Application" },
+  { question: "Domain Name Service (DNS)", answer: "Resolves hostnames, specifically Internet names. Converts domain names into IP addresses.", layer: "Application", image: "src/images/DNS.png" },
   { question: "NetBIOS", answer: "Works only in the upper layers of the OSI model and allows for an interface on separate computer to communicate over a network.", layer: "Session" },
   { question: "Dynamic Host Configuration Protocol (DHCP)", answer: "Assigns IP addresses to hosts with information provided by a server.", layer: "Application" },
   { question: "Transmission Control Protocol (TCP)", answer: "Connection-oriented protocol that provides reliable, ordered, and error-checked delivery of data between applications.", layer: "Transport" },
--- src/Card.jsx
+++ src/Card.jsx
@@ -13,13 +13,23 @@ const getColor = (layer) => {
   else if (layer === "Physical") return "#647a8d";
 }
 
-  return (
-    <div className="card" 
-        onClick={() => setIsFlipped(!isFlipped)}
-        style={{ backgroundColor: getColor(props.card.layer) }}>
-        {isFlipped ? props.card.answer : props.card.question}
+return (
+  <div className="container">
+    <div 
+      className="card" 
+      onClick={() => setIsFlipped(!isFlipped)}
+      style={{ backgroundColor: getColor(props.card.layer) }}
+    >
+      {isFlipped ? (
+        <p>{props.card.answer}</p>
+      ) : (
+        <div>
+          <p>{props.card.question}</p>
+          {props.card.image && <img src={props.card.image} alt="card" className="image" />}
+        </div>
+      )}
     </div>
-  )
+  </div>
+)
 }
-
 export default Card
